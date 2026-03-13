27fb5bbfe831403834ad88762fe9a58a03a11712
Akshay Yelne <akshayyelne000@gmail.com>
2026-03-13 15:12:24 +1100

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 # ======================================
 DB_PATH = os.getenv(
     "DB_PATH",
-    "/tmp/insurance.db"
+    os.path.join(DATA_DIR, "insurance.db")
 )
 
 # ======================================
